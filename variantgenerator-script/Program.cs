26c1056c0bd46504d69256814abf2793476a9c61
Илья Юдов <iyudov@13073f63-4252-4a2b-b7a0-fb319a79c948>
2016-03-31 18:38:16 +0000

    git-svn-id: https://svtz.ru/svn/graphlabs.tasks.variantgenerator-script@2 13073f63-4252-4a2b-b7a0-fb319a79c948

--- variantgenerator-script/Program.cs
+++ variantgenerator-script/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace variantgenerator_script
+﻿using GraphLabs.Graphs;
+using GraphLabs.Graphs.DataTransferObjects.Converters;
+using variantgenerator_script.ServiceReference;
+
+namespace variantgenerator_script
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var graphA = UndirectedGraph.CreateEmpty(3);
+            graphA.AddEdge(new UndirectedEdge(graphA.Vertices[0], graphA.Vertices[1]));
+            var data = VariantSerializer.Serialize(new IGraph[]
+            {
+                graphA,
+            });
+            var taskVariantDto = new TaskVariantDto()
+            {
+                Data = data,
+                GeneratorVersion = "1.0",
+                Id = 0,
+                TaskId = 0,
+                Number = "Вариант 1",
+                Version = null
+            };
 
+            var client = new VariantGenServiceClient();
+            client.SaveVariant(taskVariantDto, 0, false);
         }
     }
 }
